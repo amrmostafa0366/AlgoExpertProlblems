10e5617810de25cf044dbbde35e3085c690cc287
Amr Mustafa <amrmostafa0366@gmail.com>
2023-05-25 16:16:36 +0300

    MoveElementToEnd Solved--

---
+++ src/Easy/MoveElementToEnd.java
@@ -0,0 +1,29 @@
+package Easy;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class MoveElementToEnd {
+    public static List<Integer> moveElementToEnd(List<Integer> array, int toMove) {
+//        List<Integer> temp = new ArrayList<>();
+//        for (int element : array) {
+//            if (element == toMove) {
+//                temp.add(element);
+//            }
+//        }
+//        array.removeAll(temp);
+//        array.addAll(temp);
+
+        int pointer1 = 0;
+        while(pointer1<array.size()){
+            if(array.get(pointer1).equals(toMove)){
+                array.add(toMove);
+                array.remove(array.get(pointer1));
+            }
+            pointer1++;
+        }
+
+        return array;
+    }
+}
