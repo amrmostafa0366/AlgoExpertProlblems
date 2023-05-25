437a54411316485eae65ebe750afe6282f3b55c8
Amr Mustafa <amrmostafa0366@gmail.com>
2023-05-25 16:18:46 +0300

    MoveElementToEnd O(n)--

--- src/Easy/MoveElementToEnd.java
+++ src/Easy/MoveElementToEnd.java
@@ -6,23 +6,23 @@ import java.util.List;
 
 public class MoveElementToEnd {
     public static List<Integer> moveElementToEnd(List<Integer> array, int toMove) {
-//        List<Integer> temp = new ArrayList<>();
-//        for (int element : array) {
-//            if (element == toMove) {
-//                temp.add(element);
-//            }
-//        }
-//        array.removeAll(temp);
-//        array.addAll(temp);
-
-        int pointer1 = 0;
-        while(pointer1<array.size()){
-            if(array.get(pointer1).equals(toMove)){
-                array.add(toMove);
-                array.remove(array.get(pointer1));
+        List<Integer> temp = new ArrayList<>();
+        for (int element : array) {
+            if (element == toMove) {
+                temp.add(element);
             }
-            pointer1++;
         }
+        array.removeAll(temp);
+        array.addAll(temp);
+
+//        int pointer1 = 0;
+//        while(pointer1<array.size()){
+//            if(array.get(pointer1).equals(toMove)){
+//                array.add(toMove);
+//                array.remove(array.get(pointer1));
+//            }
+//            pointer1++;
+//        }
 
         return array;
     }
